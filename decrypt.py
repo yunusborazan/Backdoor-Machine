3bf246e6a47864301be2aaeb4e7cbcf9c73b596c
CUPZYY <richbanana1234@gmail.com>
2021-05-02 12:06:23 +0200

    strip the key/filepath input on the decryptor

--- decrypt.py
+++ decrypt.py
@@ -13,14 +13,14 @@ def decrypt_file(file, key):
     with open(file, 'rb') as f:
         ciphertext = f.read()
     decr = decrypt(ciphertext, key)
-    with open(os.path.basename(file)[:-12] + "_decrypted.txt", 'wb+') as f:
+    with open(os.path.basename(file)[:-4] + "_decrypted.txt", 'wb+') as f:
         f.write(decr)
-    return os.path.basename(file)[:-12] + "_decrypted.txt"
+    return os.path.basename(file)[:-4] + "_decrypted.txt"
 
 
 def main():
-    filepath = input("Input the path to the file to decrypt: ")
-    key = input("Input the decryption key: ").encode()
+    filepath = input("Input the path to the file to decrypt: ").strip(" \"")
+    key = input("Input the decryption key: ").strip(" \"").encode()
     
     filename = decrypt_file(filepath, key)
     print(f"The decryption was successful. You can now open the file: {filename}")
